43d8585586db390ecd2315c8ee5a49dce9da5309
许智杰 <976778267@qq.com>
2019-11-05 00:25:12 +0800

    - [add] 添加feign支持

--- api-notify/src/main/java/com/hailu/cloud/api/notify/NotifyApplication.java
+++ api-notify/src/main/java/com/hailu/cloud/api/notify/NotifyApplication.java
@@ -2,10 +2,14 @@ package com.hailu.cloud.api.notify;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author zhijie
  */
+@EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = {"com.hailu.cloud"})
 public class NotifyApplication {
 
