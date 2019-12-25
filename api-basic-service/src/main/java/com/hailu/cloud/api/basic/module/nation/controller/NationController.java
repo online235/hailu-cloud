dfcf4de3ce9e379a3335ef0d9bce64108dc683f2
许智杰 <976778267@qq.com>
2019-12-25 11:54:16 +0800

    - [add] 添加区域reload redis接口

--- api-basic-service/src/main/java/com/hailu/cloud/api/basic/module/nation/controller/NationController.java
+++ api-basic-service/src/main/java/com/hailu/cloud/api/basic/module/nation/controller/NationController.java
@@ -1,10 +1,14 @@
 package com.hailu.cloud.api.basic.module.nation.controller;
 
 import com.hailu.cloud.api.basic.module.nation.service.INationService;
+import com.hailu.cloud.common.constant.Constant;
+import com.hailu.cloud.common.redis.client.RedisStandAloneClient;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +32,8 @@ public class NationController {
     @Resource
     private INationService nationService;
 
+    @Autowired
+    private RedisStandAloneClient redisClient;
 
     @ApiOperation(value = "根据名称获取城市ID",notes = "{\n" +
             "  \"code\": 200,\n" +
@@ -40,4 +46,28 @@ public class NationController {
         return nationService.findByName(name);
     }
 
+    @ApiOperation(value = "把所有字典数据重新加载到Redis", notes = "<pre>" +
+            "{\n" +
+            "    'code': 200,\n" +
+            "    'message': null,\n" +
+            "    'data': ''\n" +
+            "}\n" +
+            "</pre>")
+    @GetMapping("/re-cache-redis")
+    public void reCacheToRedis() {
+        String dictKey = Constant.REDIS_KEY_DICT_CACHE + Constant.REDIS_KEY_DICT_CACHE_NATION;
+        String dictKeyDesc = Constant.REDIS_KEY_DICT_CACHE + Constant.REDIS_KEY_DICT_CACHE_NATION_DESC;
+        nationService.findAll().forEach(nation -> {
+            String value = null;
+            if(StringUtils.isNotBlank(nation.getProvince())){
+                value = nation.getProvince();
+            }else if(StringUtils.isNotBlank(nation.getCity())){
+                value = nation.getCity();
+            }else if(StringUtils.isNotBlank(nation.getDistrict())){
+                value = nation.getDistrict();
+            }
+            redisClient.hashSet(dictKey, nation.getCode(), value);
+            redisClient.hashSet(dictKeyDesc, value,nation.getCode());
+        });
+    }
 }
